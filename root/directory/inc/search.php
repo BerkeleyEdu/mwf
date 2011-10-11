d48c8989610e7d5f74c9cbf96f790826d2ff25c0
app_relmgt <app_relmgt@as-mobile-qa.IST.Berkeley.EDU>
2011-10-11 11:18:41 -0700

    changing name of directory search text box to 'search'

--- root/directory/inc/search.php
+++ root/directory/inc/search.php
@@ -1,7 +1,7 @@
 <?php
 
-if(isset($_POST['string'])){
-    $value = $_POST['string'];
+if(isset($_POST['search'])){
+    $value = $_POST['search'];
 }else if(substr($_SERVER['REQUEST_URI'], strlen($_SERVER['REQUEST_URI'])-10, 9)  == 'index.php'){
 	$value = '';
 }else if(isset($_SESSION['lastsearch'])){
@@ -16,6 +16,6 @@ if(isset($_POST['string'])){
     <h1 class="content-first">Search Directory</h1>
     
     <form id="search-form" action="results.php" method="post">
-        <input type="text" name="string" id="search-box" value="<?php echo $value; ?>"  style='width:73%; max-width:300px' placeholder="By name, phone, or email"/> <input id="search-button" type="submit" name="submit" value="Search" class="form-last" />
+        <input type="text" name="search" id="search-box" value="<?php echo $value; ?>"  style='width:73%; max-width:300px' placeholder="By name, phone, or email"/> <input id="search-button" type="submit" name="submit" value="Search" class="form-last" />
     </form>
 </div>
