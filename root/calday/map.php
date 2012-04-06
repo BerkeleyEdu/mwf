e25644b01f52e282f608b1fc145a5027b3febaa6
app_relmgt <app_relmgt@as-mobile-qa.IST.Berkeley.EDU>
2012-04-06 12:48:39 -0700

    updating Cal Day map to use new geolocation function

--- root/calday/map.php
+++ root/calday/map.php
@@ -134,12 +134,14 @@ $locations = new Locations('http://www.berkeley.edu/data/ucb_calday_map_coordina
             ?>
 			if (mwf.touch.geolocation.isSupported() )
 			 {
-				mwf.touch.geolocation.getPosition(
+				//mwf.touch.geolocation.getPosition(
+				mwf.touch.geolocation.getCurrentPosition(
 						function(pos)
 						{  						
 						map.addLocation('Your current location', pos['latitude'], pos['longitude'], "http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png"); 
 						},
-						function(err){ alert("Err:"+err); }
+						//function(err){ alert("Err:"+err); }
+						function(err){if(err.code!=err.PERMISSION_DENIED) alert("Err:"+err.message);}
 					)
 			 }
         </script>
