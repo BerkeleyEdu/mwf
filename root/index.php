5e279c0297caee606a752878fc977e518e3f4dcd
app_relmgt <app_relmgt@as-mobile-qa.IST.Berkeley.EDU>
2012-04-23 09:31:46 -0700

    Fixing typo in Events link

--- root/index.php
+++ root/index.php
@@ -189,7 +189,7 @@ if (Classification::is_full() )
 		$image = '';
 	}
 	$events_title = $image . $simple_xml->Events->Event->EventTitle;	
-	$events_link = 'http://events.berkeley.edu/mobile/"';
+	$events_link = 'http://events.berkeley.edu/mobile/';
 	$image_wrapper = true;	
 	
 	// Get Cal Day
